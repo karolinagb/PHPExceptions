4d350a0ae6037dacc431d3dc26d5554b6148a221
x <karolina_bp@hotmail.com>
2022-06-28 13:55:35 -0300

    Pilha de execução e debugger

--- pilha.php
+++ pilha.php
@@ -1,9 +1,19 @@
 <?php
 
+//um arquivo php tem duas principais formas de instrução:
+
+//instruções de declaração de símbolos = criação de classes, funções
 function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
     funcao2();
+
+    //Quero tirar uma foto de como está a pilha de execução nessa linha
+    //debug_backtrace() - trilha da pilha que foi executado ate aq
+    // var_dump(debug_backtrace());
+    //Alternativa ao var_dump:
+    // print_r(debug_backtrace());
+
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
@@ -16,6 +26,7 @@ function funcao2()
     echo 'Saindo da função 2' . PHP_EOL;
 }
 
+//instruções de execução = vai ser executado quando executarmos o arquivo
 echo 'Iniciando o programa principal' . PHP_EOL;
 funcao1();
 echo 'Finalizando o programa principal' . PHP_EOL;
