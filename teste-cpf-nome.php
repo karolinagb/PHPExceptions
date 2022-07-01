9a9eaa02ab7c9396e49aed0c9b9289e62edd38d7
x <karolina_bp@hotmail.com>
2022-07-01 17:26:40 -0300

    Validado CPF e Nome com exceções

---
+++ teste-cpf-nome.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'autoload.php';
+
+use Alura\Banco\Modelo\Conta\Titular;
+use Alura\Banco\Modelo\CPF;
+use Alura\Banco\Modelo\Endereco;
+
+try{
+    $cpf = new CPF('123');
+}
+catch(InvalidArgumentException){
+    echo "CPF Inválido!" . PHP_EOL;
+    echo "Digite um cpf como do exemplo: 111.222.333-99" . PHP_EOL;
+}
+
+try{
+    $pessoa = new Titular(new CPF('123.456.789-99'), 'Kar', new Endereco('A', 'B', 'C', 'D'));
+}
+catch(DomainException){
+    echo "Nome precisa ter pelo menos 5 caracteres";
+}
